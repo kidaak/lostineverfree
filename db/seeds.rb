3dd4b492082191cc4d891f6ca2b25ea1ca1e7913
Desmond Rawls <captaingrover@gmail.com>
2013-08-10 14:00:35 -0400

    Update seed file so Everfree is now a 3x3 map.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Setting.create!(in_everfree: true, north: 3, south: 2, east: 6, west: 5, name: "Everfree Mirror Pool", img_url:"http://images2.wikia.nocookie.net/__cb20121117175606/mlp/images/7/70/Pinkie_sees_the_Mirror_Pool_S3E03.png")
+Setting.create!(in_everfree: true, north: 1, south: nil, east: 9, west: 8, name: "Everfree Row Of Trees By Day", img_url:"http://fc09.deviantart.net/fs71/i/2013/027/c/c/everfree_forest_corridor_background_by_golliattaillog-d5t069n.jpg")
+Setting.create!(in_everfree: true, north: nil, south: 1, east: 7, west: 4, name: "Everfree Clearing By Night", img_url:"http://th08.deviantart.net/fs70/PRE/i/2012/179/a/9/nightmare_night_in_the_everfree_forest_by_hellswolfeh-d555w4a.png")
+Setting.create!(in_everfree: true, north: nil, south: 5, east: 3, west: nil, name: "Everfree Bridge", img_url:"http://www.fimfiction-static.net/images/story_images/22445.png")
+Setting.create!(in_everfree: true, north: 4, south: 8, east: 1, west: nil, name: "Everfree Row of Trees By Night", img_url:"http://fc04.deviantart.net/fs71/i/2012/085/9/0/free_background_everfree_forest_by_darkbx-d4tnbm6.png")
+Setting.create!(in_everfree: true, north: 7, south: 9, east: nil, west: 1, name: "Everfree By The River", img_url:"http://fc06.deviantart.net/fs70/i/2012/301/2/5/background___everfree_forest__river__by_hawk9mm-d5j7hrq.png")
+Setting.create!(in_everfree: true, north: nil, south: 6, east: nil, west: 3, name: "Everfree Broken Bridge", img_url:"http://fc01.deviantart.net/fs70/f/2011/310/e/8/everfree_forest_background_by_somepony-d4fdt4a.png")
+Setting.create!(in_everfree: true, north: 5, south: nil, east: 2, west: nil, name: "Everfree Nice Walk", img_url:"http://fc09.deviantart.net/fs71/i/2013/198/8/b/everfree_forest_path_by_rainbowrage12-d6dwou8.png")
+Setting.create!(in_everfree: true, north: 6, south: nil, east: nil, west: 2, name: "Everfree Trees With Eyes", img_url:"http://fc07.deviantart.net/fs70/i/2012/309/b/5/everfree_forest_entrance_v2_by_aaastudios-d5k2rfz.png")
+
 
 Pony.create!(name: "Queen Chrysalis", img_url: "http://fc06.deviantart.net/fs71/i/2012/316/d/7/queen_chrysalis_by_proenix-d5ksgw5.png")
 Pony.create!(name: "Twilight Sparkle", img_url: "http://www.hasbro.com/mylittlepony/images/carousel/twilight-sparkle-slide.png")
@@ -21,10 +24,5 @@ Setting.create!(in_everfree: false, name: "Canterlot Midday", img_url:"http://im
 Setting.create!(in_everfree: false, name: "Canterlot Winter", img_url:"http://images1.wikia.nocookie.net/__cb20111219011851/mlp/images/9/98/Canterlot_Entrance_S2E11.png")
 Setting.create!(in_everfree: false, name: "Canterlot Street By Night", img_url:"http://fc00.deviantart.net/fs71/i/2012/166/e/7/ye_olde_canterlot_by_spiritto-d53lwox.png")
 Setting.create!(in_everfree: false, name: "Canterlot Street By Day", img_url:"http://images3.wikia.nocookie.net/__cb20121212070759/mlp/images/e/e0/A_Canterlot_street_S2E9.png")
-Setting.create!(in_everfree: true, north: 9, south: nil, east: 7, west: nil, name: "Everfree Mirror Pool", img_url:"http://images2.wikia.nocookie.net/__cb20121117175606/mlp/images/7/70/Pinkie_sees_the_Mirror_Pool_S3E03.png")
-Setting.create!(in_everfree: true, north: 8, south: nil, east: nil, west: 6, name: "Everfree Row Of Trees By Day", img_url:"http://fc09.deviantart.net/fs71/i/2013/027/c/c/everfree_forest_corridor_background_by_golliattaillog-d5t069n.jpg")
-Setting.create!(in_everfree: true, north: nil, south: 7, east: nil, west: 9, name: "Everfree Clearing By Night", img_url:"http://th08.deviantart.net/fs70/PRE/i/2012/179/a/9/nightmare_night_in_the_everfree_forest_by_hellswolfeh-d555w4a.png")
-Setting.create!(in_everfree: true, north: nil, south: 6, east: 8, west: nil, name: "Everfree Bridge", img_url:"http://www.fimfiction-static.net/images/story_images/22445.png")
-
 
 
