d80fb93d990b0a2c5e7d8230578200791f0d32a8
Desmond Rawls <captaingrover@gmail.com>
2013-08-11 17:11:25 -0400

    debug messy rebase

--- db/migrate/20130811171858_add_pony_positioning_to_settings.rb
+++
@@ -1,7 +0,0 @@
-class AddPonyPositioningToSettings < ActiveRecord::Migration
-  def change
-    add_column :settings, :pony_position_left, :string
-    add_column :settings, :pony_position_top, :string
-    add_column :settings, :pony_width, :string
-  end
-end
