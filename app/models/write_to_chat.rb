195f69c950da5ee4dcd884a53dc473bd512beb7f
Desmond Rawls <captaingrover@gmail.com>
2013-08-21 12:13:30 -0400

    Rename localhost to ip address for faye.

--- app/models/write_to_chat.rb
+++ app/models/write_to_chat.rb
@@ -1,6 +1,6 @@
 class WriteToChat
   def self.push_message(content, heroine, outgoing)
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("http://198.211.98.18:9292/faye")
     data = {:content => content, :heroine => heroine, :outgoing => outgoing}
     message = {:channel => "/receive", :data => data}
     Net::HTTP.post_form(uri, :message => message.to_json)
