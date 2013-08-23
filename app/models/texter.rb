1f069b3ce8d8c80bab6d57f73e93f7b1374f3d05
Desmond Rawls <captaingrover@gmail.com>
2013-08-23 16:01:14 -0400

    Make the texting list me, nina, and lia

--- app/models/texter.rb
+++ app/models/texter.rb
@@ -1,5 +1,5 @@
 class Texter
-  @@family = ["ME", "DELL", "LIA"]
+  @@family = ["ME", "LIA", "NINA"]
   def self.send_with_twilio(speaker, content)
     body = speaker + ": " + content
     @@family.each do |family_member|
