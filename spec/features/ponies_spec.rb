d510aedb8d730904e968c91aaa4ab09ddd1a5b0a
Desmond Rawls <captaingrover@gmail.com>
2013-09-20 13:13:58 -0400

    pass test for adding a pony to the list

--- spec/features/ponies_spec.rb
+++ spec/features/ponies_spec.rb
@@ -12,6 +12,20 @@ describe "ponies", type: :request, js: true do
 
     within 'span.last' do
       page.should have_content pony.name
+      page.should have_xpath("//img[@src=\"#{pony.img_url}\"]")
+    end
+
+  end
+
+  it 'adds a pony to the list' do
+
+    fill_in 'new_pony_name', :with => "Applejack"
+    fill_in 'new_pony_img_url', :with => "http://fc03.deviantart.net/fs71/i/2012/218/0/0/applejack_leaning_by_vladimirmacholzraum-d53ne7e.png"
+    click_on 'add-pony'
+
+    within 'span.last' do
+      page.should have_content "Applejack"
+      page.should have_xpath("//img[@src=\"http://fc03.deviantart.net/fs71/i/2012/218/0/0/applejack_leaning_by_vladimirmacholzraum-d53ne7e.png\"]")
     end
 
   end
